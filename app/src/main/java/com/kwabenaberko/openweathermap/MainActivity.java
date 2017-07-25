5d10a437a18d852319aaede80f6f1731bf77bef3
KwabenBerko <kwabenaberko95@gmail.com>
2017-07-25 22:37:35 +0000

    Added Sample

--- app/src/main/java/com/kwabenaberko/openweathermap/MainActivity.java
+++ app/src/main/java/com/kwabenaberko/openweathermap/MainActivity.java
@@ -10,22 +10,36 @@ import com.kwabenaberko.openweathermaplib.models.CurrentWeather;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         OpenWeatherMapHelper helper = new OpenWeatherMapHelper();
-        helper.setAppId("e1e88645e0c18f577551c16afbe063fd");
-        helper.getCurrentWeatherByZipCode("97205", new OpenWeatherMapHelper.CurrentWeatherCallback() {
+        helper.setAppId(getString(R.string.OPEN_WEATHER_MAP_APP_ID));
+        helper.setUnits("imperial");
+
+        /*
+        This Example Only Shows how to get current weather by city name
+        Check the docs for more methods [https://github.com/KwabenBerko/OpenWeatherMap-Android-Library/]
+        */
+        helper.getCurrentWeatherByCityName("Accra", new OpenWeatherMapHelper.CurrentWeatherCallback() {
             @Override
             public void onSuccess(CurrentWeather currentWeather) {
-                Toast.makeText(MainActivity.this, currentWeather.getName(), Toast.LENGTH_SHORT).show();
+                Log.v(TAG,
+                        "Coordinates: " + currentWeather.getCoord().getLat() + ", "+currentWeather.getCoord().getLat() +"\n"
+                                +"Weather Description: " + currentWeather.getWeatherArray().get(0).getDescription() + "\n"
+                                +"Temperature: " + currentWeather.getMain().getTempMax()+"\n"
+                                +"Wind Speed: " + currentWeather.getWind().getSpeed() + "\n"
+                                +"City, Country: " + currentWeather.getName() + ", " + currentWeather.getSys().getCountry()
+                );
             }
 
             @Override
             public void onFailure(Throwable throwable) {
-                Log.v("RES", throwable.getMessage());
+                Log.v(TAG, throwable.getMessage());
             }
         });
     }
